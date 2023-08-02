c107a6d061e55b3c040ad94d1150357ba0f74835
Vitor Lopes Nunes <vitorlopes040805@gmail.com>
2023-08-02 08:56:53 -0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Cardapio from './pages/Cardapio/index';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Cardapio/> {/* <- Aparece na tela */}
+  </React.StrictMode>
+);
