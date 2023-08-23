68ccd00f39f1116089a576724b97848f3af936d0
Vitor Lopes Nunes <vitorlopes040805@gmail.com>
2023-08-23 08:56:20 -0300

    header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,12 +1,20 @@
 import styles from './Cardapio.module.scss'
-import Logo from '../../assets/logo.svg'
+import Logo from 'assets/logo2.png'
 
 export default function Cardapio(){
     return(
         <main>
             <nav className={styles.menu}>
-                <img src={Logo} alt="Logo" />
+                <img src={Logo} alt="Logo" className={styles.logo}/>
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    MANGIA CHE TE FA BENE
+                </div>
+            </header>
+            <section>
+                <h3>Cardápio</h3>
+            </section>
         </main>
     );
 }
